e3fa227d1f8c0e26cd45dde087f129f2248a9092
Mohammed sinan <mohammedbusiness1099@gmail.com>
2021-06-04 14:19:38 +0300

    antd pre build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import HeaderApp from './Components/HeaderApp'
+import "./App.css"
+import SectionOne from './Components/SectionOne'
+import SectionTwo from './Components/SectionTwo'
+import SectionThree from './Components/SectionThree'
+import SectionFour from './Components/SectionFour'
+import SectionFive from './Components/SectionFive'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderApp />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
     </div>
-  );
+  )
 }
-
-export default App;
